b11cdb214bd52cd7605ab24fb0f02a141bae5b59
Suzi Nogueira <asuzinogueira@gmail.com>
2021-12-08 00:10:54 -0300

    implementing SOLID

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,12 +1,8 @@
 import { Category } from "../model/Category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository";
 
-//DID - Data transfer object
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
 
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
